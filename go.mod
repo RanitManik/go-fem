db014ea0274d2d3e42a3427da34d5107b49188d4
RanitManik <138437760+RanitManik@users.noreply.github.com>
2025-03-31 17:12:33 +0530

    added files

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/RanitManik/go-fem
+
+go 1.24.1
+
+require github.com/go-chi/chi/v5 v5.2.1 // indirect
---
+++ internal/routes/routes.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"github.com/RanitManik/go-fem/internal/app"
+	"github.com/go-chi/chi/v5"
+)
+
+func SetupRoutes(app *app.Application) *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Get("/health", app.HealthCheck)
+	return r
+}
